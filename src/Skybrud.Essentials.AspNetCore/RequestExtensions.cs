34ed662b7e6b3e067b4ba9a79623ba7de7dc9025
Anders Bjerner <abjerner@skybrud.dk>
2021-08-15 01:27:56 +0100

    Initial commit

---
+++ src/Skybrud.Essentials.AspNetCore/RequestExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Skybrud.Essentials.AspNetCore {
+    
+    /// <summary>
+    /// Various extension methods for working with <see cref="HttpRequest"/>.
+    /// </summary>
+    public static class RequestExtensions {
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> of the specified <paramref name="request"/>.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>An instance of <see cref="Uri"/>.</returns>
+        public static Uri GetUri(this HttpRequest request) {
+            return new UriBuilder {
+                Scheme = request.Scheme,
+                Host = request.Host.Host,
+                Port = request.Host.Port ?? (request.Scheme == "https" ? 80 : 443),
+                Path = request.Path,
+                Query = request.QueryString.ToUriComponent()
+            }.Uri;
+        }
+
+    }
+
+}
