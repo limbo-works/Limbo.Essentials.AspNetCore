377f81c887dc62737c353fb070bcdc5c9d6cbe34
Anders Bjerner <abjerner@skybrud.dk>
2021-11-25 19:02:32 +0100

    Added new code for our SPA package

---
+++ src/Skybrud.Essentials.AspNetCore/QueryStringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Primitives;
+using Skybrud.Essentials.Strings.Extensions;
+using System.Linq;
+
+namespace Skybrud.Essentials.AspNetCore {
+    
+    /// <summary>
+    /// Various extension methods for working with ASP.NET Core query strings.
+    /// </summary>
+    public static class QueryStringExtensions {
+        
+        /// <summary>
+        /// Converts the specified <paramref name="values" /> to an <see cref="bool" /> value.
+        /// </summary>
+        /// <param name="values">The values to be converted.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static int ToInt32(this StringValues values) {
+            string input = values.FirstOrDefault();
+            return input?.ToInt32() ?? 0;
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="values" /> to an <see cref="bool" /> value. If the conversion fails,
+        /// <paramref name="fallback" /> is returned instead.
+        /// </summary>
+        /// <param name="values">The values to be converted.</param>
+        /// <param name="fallback">The fallback value.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static int ToInt32(this StringValues values, int fallback) {
+            string input = values.FirstOrDefault();
+            return input?.ToInt32(fallback) ?? fallback;
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="values" /> to a <see cref="bool" /> value. If the conversion fails,
+        /// <c>false</c> is returned instead.
+        /// </summary>
+        /// <param name="values">The values to be converted.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static bool ToBoolean(this StringValues values) {
+            string str = values.FirstOrDefault();
+            return str != null && str.ToBoolean();
+        }
+
+        /// <summary>
+        /// Converts the specified <paramref name="values" /> to a <see cref="bool" /> value. If the conversion fails,
+        /// <paramref name="fallback" /> is returned instead.
+        /// </summary>
+        /// <param name="values">The values to be converted.</param>
+        /// <param name="fallback">The fallback value.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static bool ToBoolean(this StringValues values, bool fallback) {
+            string str = values.FirstOrDefault();
+            return str?.ParseBoolean(fallback) ?? fallback;
+        }
+
+    }
+
+}
--- src/Skybrud.Essentials.AspNetCore/RequestExtensions.cs
+++ src/Skybrud.Essentials.AspNetCore/RequestExtensions.cs
@@ -22,6 +22,60 @@ namespace Skybrud.Essentials.AspNetCore {
                 Query = request.QueryString.ToUriComponent()
             }.Uri;
         }
+        
+        /// <summary>
+        /// Returns the value of the <c>Accept</c> HTTP header
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The header value.</returns>
+        public static string GetAcceptTypes(this HttpRequest request) {
+            return request?.HttpContext.Request?.Headers["Accept"];
+        }
+        
+        /// <summary>
+        /// Returns the value of the <c>Accept-Encoding</c> HTTP header
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The header value.</returns>
+        public static string GetAcceptEncoding(this HttpRequest request) {
+            return request?.HttpContext.Request?.Headers["Accept-Encoding"];
+        }
+        
+        /// <summary>
+        /// Returns the value of the <c>Accept-Language</c> HTTP header
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The header value.</returns>
+        public static string GetAcceptLanguage(this HttpRequest request) {
+            return request?.HttpContext.Request?.Headers["Accept-Language"];
+        }
+
+        /// <summary>
+        /// Gets the remote address of the specified <paramref name="request"/>.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The remote address.</returns>
+        public static string GetRemoteAddress(this HttpRequest request) {
+            return request?.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+        
+        /// <summary>
+        /// Returns the value of the <c>Referer</c> HTTP header
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The header value.</returns>
+        public static string GetReferrer(this HttpRequest request) {
+            return request?.HttpContext.Request?.Headers["Referer"];
+        }
+
+        /// <summary>
+        /// Gets the user agent of the specified <paramref name="request"/>.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>The user agent.</returns>
+        public static string GetUserAgent(this HttpRequest request) {
+            return request?.HttpContext.Request?.Headers["User-Agent"];
+        }
 
     }
 
