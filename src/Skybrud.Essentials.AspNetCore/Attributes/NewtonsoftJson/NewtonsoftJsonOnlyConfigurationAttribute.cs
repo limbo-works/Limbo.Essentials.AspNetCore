16f7f3728e5e3e3171872de48fe7f77cf99c1385
Anders Bjerner <abjerner@skybrud.dk>
2022-06-30 14:02:35 +0200

    Introduced new "NewtonsoftJsonOnlyConfigurationAttribute" class and related logic
    The attribute may be added to API controllers, in which case the output will be serialized using Newtonsoft.Json instead of the default System.Text.Json.

---
+++ src/Skybrud.Essentials.AspNetCore/Attributes/NewtonsoftJson/NewtonsoftJsonOnlyConfigurationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Skybrud.Essentials.AspNetCore.Filters.NewtonsoftJson;
+
+#pragma warning disable CS1591
+
+namespace Skybrud.Essentials.AspNetCore.Attributes.NewtonsoftJson {
+
+    /// <summary>
+    /// When added to an API controller, the output will be serialized to JSON using <strong>Newtonsoft.Json</strong>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class NewtonsoftJsonOnlyConfigurationAttribute : TypeFilterAttribute {
+
+        public NewtonsoftJsonOnlyConfigurationAttribute() : base(typeof(NewtonsoftJsonOnlyConfigurationFilter)) {
+            Order = 1;
+        }
+
+    }
+
+}
